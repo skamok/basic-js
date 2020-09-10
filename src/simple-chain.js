863ec106ecabb511a23ff6407b4c65eb9b85ceee
skamok <43462201+skamok@users.noreply.github.com>
2020-09-10 20:45:01 +0300

    Update simple-chain.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  arr : [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    //console.log('addlink='+ value);
+    if (value === undefined) {
+      this.arr.push(' ');
+    } else {
+      this.arr.push(`${value}`);
+    } 
+    //console.log('ADDarr='+this.arr );   
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    //console.log('removeLink='+ value);
+    if ( (typeof(position) !== 'number' ) || (position < 1) || (position > this.arr.length) ) {this.arr = []; throw Error('Error');};
+    this.arr.splice(position-1, 1);
+    //console.log('REMarr='+this.arr );
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (this.arr.length> 1) this.arr.reverse();
+    //console.log('REVarr='+this.arr );
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    //console.log('Finish='+this.arr );
+    let str = '';
+    if (this.arr.length === 0) { str = ''}
+     else {
+    this.arr.forEach((el, ind, arr) => {
+      if (arr.length === 0) { this.arr = []; str = '';}
+      if ((ind === 0) || (ind < (arr.length-1))) 
+        { if (arr.length ===1 ) {str = `${str}( ${el} )`} else { str = `${str}( ${el} )~~`} }
+        else
+        {str = `${str}( ${el} )`}
+    });
+  }
+    this.arr = [];
+    return str;
   }
 };
+
 
 module.exports = chainMaker;
