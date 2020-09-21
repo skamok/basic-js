1719ad4a77f1406eb6938bf28a2dcc42d8e33255
skamok <43462201+skamok@users.noreply.github.com>
2020-09-21 15:03:54 +0300

    feat: add vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,58 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+constructor(mode) {
+        if (mode === undefined) { 
+          this.mode = true;} else {
+            this.mode = mode; 
+          }
+        }
+    encrypt(message, key) {
+    //throw new CustomError('Not implemented');
+    let k = 0;    
+    let shifr = '';
+    let res = 0;   
+    message = message.toUpperCase(); 
+    key = key.toUpperCase();
+    for (let i = 0; i < message.length; i++) {      
+      if (k>(key.length-1)) {k = 0;};
+      if (message[i].search(/[A-Za-z]/) === -1) {        
+        shifr = shifr + message[i];
+      } else {        
+        res = ((key.charCodeAt(k) + message.charCodeAt(i)) % 26) + 65;        
+        shifr = shifr + String.fromCharCode(res);
+        //console.log('message='+message.charCodeAt(i)+' key='+key.charCodeAt(k)+' res='+res+' reslit='+String.fromCharCode(res));
+        k++;
+      }
+    }
+    if (this.mode === true) {
+      return(shifr);} else {
+        return(shifr.split('').reverse().join(''));
+      }
+  }  
+
+  decrypt(message, key) {
+    //throw new CustomError('Not implemented');
+    let k = 0;    
+    let shifr = '';
+    let res = 0;   
+    message = message.toUpperCase(); 
+    key = key.toUpperCase();
+    for (let i = 0; i < message.length; i++) {      
+      if (k>(key.length-1)) {k = 0;};
+      if (message[i].search(/[A-Za-z]/) === -1) {        
+        shifr = shifr + message[i];
+      } else {        
+        res = ((26 + message.charCodeAt(i) - key.charCodeAt(k)) % 26) +65 ;        
+        shifr = shifr + String.fromCharCode(res);
+        //console.log('message='+message.charCodeAt(i)+' key='+key.charCodeAt(k)+' res='+res+' reslit='+String.fromCharCode(res));
+        k++;
+      }
+    }
+    if (this.mode === true) {
+      return(shifr);} else {
+        return(shifr.split('').reverse().join(''));
+      }
   }
 }
 
